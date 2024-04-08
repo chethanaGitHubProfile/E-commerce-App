58aba08044b86dafe5539036b235380af535ab5f
chethanaGitHubProfile <155297746+chethanaGitHubProfile@users.noreply.github.com>
2024-04-08 08:51:51 +0530

    Reserve,Grn,payment,Ctrl changes

--- products/app/controller/reserve-ctrl.js
+++ products/app/controller/reserve-ctrl.js
@@ -2,7 +2,9 @@ const { validationResult } = require("express-validator");
 const Reserve = require("../models/reserve-model");
 const Product = require("../models/product-model");
 const { format } = require("date-fns");
+const cron = require("node-cron");
 const _ = require("lodash");
+const { productId } = require("../validations/grn-validation");
 const reserveCtrl = {};
 
 reserveCtrl.create = async (req, res) => {
@@ -15,19 +17,21 @@ reserveCtrl.create = async (req, res) => {
     //console.log(body);
     const reserve = new Reserve(body);
     //console.log(reserve);
+    await reserve.save();
+
+    //if all validation tehn we are updating the "quantity" to product "stock"
     const { productId, reserveQuantity } = req.body;
     const product = await Product.findByIdAndUpdate(
       productId,
       {
-        $inc: { stock: reserveQuantity },
+        $inc: { reserveStock: reserveQuantity },
       },
       { new: true }
     );
-    reserve.product = product;
-    await reserve.save();
+    reserve.productId = product;
     return res.status(200).json(reserve);
   } catch (err) {
-    console.log(err);
+    //console.log(err);
     return res.status(500).json("Internal server error");
   }
 };
@@ -47,17 +51,14 @@ reserveCtrl.update = async (req, res) => {
     const { productId, reserveQuantity } = req.body;
     const product = await Product.findOneAndUpdate(
       productId,
-      {
-        $inc: { stock: reserveQuantity },
-      },
+      { reserveStock: reserveQuantity },
       { new: true }
     );
     reserve.product = product;
-    console.log(reserve);
+    //console.log(reserve);
     res.json(reserve);
   } catch (err) {
     res.json(err);
-    console.log(err);
   }
 };
 
@@ -73,10 +74,27 @@ reserveCtrl.softDelete = async (req, res) => {
       },
       { new: true }
     );
+
+    if (!reserve) {
+      return res.status(400).json({ message: "Given Reserve Id not found" });
+    }
+
+    //fetching productId from reserve object
+    const productId = reserve.productId;
+    //console.log(productId);
+    const product = await Product.findOneAndUpdate(
+      productId,
+      { reserveStock: 0 },
+      { new: true }
+    );
+    //console.log(product);
+
+    reserve.product = product;
     const filterReserve = {
       _id: reserve._id,
       isDeleted: reserve.isDeleted,
       status: reserve.status,
+      productId: reserve.productId,
     };
     res.json(filterReserve);
   } catch (err) {
@@ -87,7 +105,11 @@ reserveCtrl.softDelete = async (req, res) => {
 reserveCtrl.ended = async (req, res) => {
   const id = req.params.id;
   try {
-    const reserve = await Reserve.findById(id);
+    const reserve = await Reserve.findByIdAndUpdate(
+      id,
+      { status: "Ended" },
+      { new: true }
+    );
     if (!reserve) {
       return res.status(400).json({ message: "Given Reserve Id not found" });
     }
@@ -99,9 +121,26 @@ reserveCtrl.ended = async (req, res) => {
     } else {
       reserve.status = "Ended";
     }
-    await reserve.save();
-    console.log(reserve);
-    res.status(200).json(reserve);
+
+    //make reserveStock from product object to Zero
+    //find productId from reserve object
+    const productId = reserve.productId;
+
+    //now make that producId of that reserve Id to Zero
+    const product = await Product.findOneAndUpdate(
+      productId,
+      { reserveStock: 0 },
+      { new: true }
+    );
+    reserve.product = product;
+
+    const filterReserve = {
+      _id: reserve.productId,
+      status: reserve.status,
+      isDeleted: reserve.isDeleted,
+      productId: reserve.productId,
+    };
+    res.status(200).json(filterReserve);
   } catch (err) {
     res.json(err);
   }
